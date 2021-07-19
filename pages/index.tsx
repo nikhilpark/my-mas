31688df403d76dab431d7a06c993ab43f716a67c
Nikhil Kumar <1703322.it.cec@cgc.edu.in>
2021-07-19 19:31:46 +0530

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+import React, { useState } from 'react'
+
+
+export default function Home() {
+
+
+  return (
+    <>
+    <Head>
+      <title>
+        Yo!
+      </title>
+    </Head>
+    <div>
+      <p className="text-red-400" >Yoo </p>
+    </div>
+    </>
+  )
+}
